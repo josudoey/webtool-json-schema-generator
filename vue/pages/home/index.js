aaee618806bb04db525d9dc06abdef122c815108
Joey Fang <josudoey@gmail.com>
2021-09-01 00:07:33 +0800

    feat: support json5&yaml

--- vue/pages/home/index.js
+++ vue/pages/home/index.js
@@ -1,16 +1,43 @@
 import template from './template.pug'
 import schemaGenerator from '@stoplight/json-schema-generator'
+import YAML from 'js-yaml'
+import pick from 'lodash/pick'
+import JSON5 from 'json5'
+const persistKey = 'state'
+const persistProps = ['type', 'sample', 'result']
 export default {
   template,
   data: () => {
     return {
-      sample: '',
+      type: 'json',
+      sample: '{"hello":"world"}',
       result: ''
     }
   },
+  mounted () {
+    this.load()
+    this.translate()
+  },
+  updated () {
+    this.save()
+  },
   methods: {
+    save: function () {
+      const persisValue = JSON.stringify(pick(this.$data, persistProps))
+      window.sessionStorage.setItem(persistKey, persisValue)
+    },
+    load () {
+      const state = window.sessionStorage.getItem(persistKey)
+      if (!state) {
+        return
+      }
+      const persistState = pick(JSON.parse(state), persistProps)
+      Object.assign(this.$data, persistState)
+    },
     translate () {
-      this.result = JSON.stringify(schemaGenerator(JSON.parse(this.sample)), null, 4)
+      const schema = schemaGenerator(JSON5.parse(this.sample))
+      const result = (this.type === 'json') ? JSON.stringify(schema, null, 4) : YAML.dump(schema)
+      this.result = result
     }
   }
 }
